fad3d98f5912e1168b834dbc40f8c93c52c648d3
Tega Ogheneovo <ogheneovoreal@gmail.com>
2023-11-01 22:27:14 +0000

    mvp 3

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,13 +22,18 @@ const { uploadFileTos3, getFileStreamFroms3 } = require("./s3");
 
 if (process.env.APP_STATE === "dev") {
   const corsOptions = {
-    origin: "http://localhost:3000",
+    //// origin: "http://localhost:3000",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  };
+  app.use(cors());
+} else {
+  const corsOptions = {
+    origin: "http://clikbate.com",
     credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus: 200,
   };
   app.use(cors(corsOptions));
-} else {
-  app.use(cors());
 }
 
 app.use(express.json({ limit: "400mb" }));
